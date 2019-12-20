5ca7b9ffaab28a2b8e0e5ab53d520c0ec64ff77b
Tudouxks <2908509482@qq.com>
2019-12-21 00:32:09 +0800

    add login function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
 
-//  手动配置ELement-UI
+// 导入字体图标
+import './assets/css/iconfont.css'
+
+// 导入element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+//  手动配置ELement-UI
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
